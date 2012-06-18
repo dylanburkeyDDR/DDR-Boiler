c3966ebb1412dcd42234123de6d5e8de3398cc72
Dylan Burkey <dburkey@ddr.com>
2012-06-18 10:55:36 -0400

    v1

---
+++ index.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title> | Retail Space For Lease | DDR</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width">
+	<link rel="stylesheet/less" type="text/css" href="css/styles.less">  
+	<link rel="stylesheet" href="css/style.css">
+	<script src="js/less-1.3.0.min.js" type="text/javascript"></script>
+	<script src="js/libs/modernizr-2.5.3.min.js"></script>
+</head>
+<body>
+  <!--[if lt IE 7]><p class=chromeframe>Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
+  <header>
+	 <a title='DDR' href="/default.aspx"><img src="img/ddr_main.gif" title='DDR' alt="DDR" width="204" height="124" id="logo" /></a> <img title='Think Retail. Create Value.' src="img/header-tagline.gif" alt="Think Retail. Create Value." width="201" id="header-tagline" />
+    <nav role='navigation'>
+      <div id="menu"> 
+      <a title='Company' href="/company.aspx" id="company-menu"></a> 
+      <a title='Properties' href="/listings/map-of-properties.aspx" id="properties-menu"></a> 
+      <a title='Investor Relations' href="http://ir.ddr.com" id="investor-menu"></a>
+       <a title='Contact Us' href="/contactus.aspx" id="contact-menu"></a> 
+       </div>
+    </nav> 
+    <div id="search" role='search'>
+        <input type="text" id="search-field" class="thunder-self-labeled" value="search keyword" />
+        <input type="image" onClick="DDR.searchSite();return false;" id="search-button" src="img/top-search-button.gif" alt="Search" />
+    </div>
+    </div>
+    <div id="locator-expander"> <img src="img/property-locator.gif" />
+      <div id="locator-arrows"> </div>
+    </div>
+    <div id="breadcrumbs"> <a href="/default.aspx">Home page</a> 
+    <span class="breadcrumb-arrows">&gt;&gt;</span> 
+    <a title='Company' href="/company.aspx">Company</a> 
+    <span class="breadcrumb-arrows">&gt;&gt;</span>
+     <a title='Careers' class="current-page" href="/careers.aspx" >Careers</a> </div>
+</header>
+  <div role="main">
+
+  </div>
+  <footer>
+  	<div id="footer">
+
+            	<img src="img/listed-nyse.gif" alt="DDR listed NYSE" title="DDR Listed NYSE" id="listed" />
+
+            <address> <div class=vcard itemscope itemtype="http://data-vocabulary.org/Organization"> <span class="fn org" itemprop=name>DDR Corp.</span> <div class=adr itemprop=address itemscope itemtype="http://data-vocabulary.org/Address"> <span class=street-address itemprop=street-address>3300 Enterprise Parkway</span><br/> <span class=locality itemprop=locality>Beachwood </span> <span class=region itemprop=region>Ohio</span> <span class=postal-code itemprop=postal-code title=44129>44129</span> </div> <span class=geo> <span class=latitude><span class=value-title title="41.469068"></span></span> <span class=longitude><span class=value-title title="-81.496196"></span></span> </span> <span itemprop=tel class=tel id=skypenumber>877.225.5337</span> </div> </address>
+             <div class="footer-content-block footer-medium">Copyright 2011<br /><a  tile='Privacy Policy' href="/privacy.aspx">Privacy Policy</a><br /><a title='DDR Terms Of Use' href="/terms-of-use.aspx">Terms of Use</a></div>
+
+                <div class="footer-content-block footer-medium"><div id="phonenumber">877.225.5337</div><br /><a title='DDR Site Map' href="/sitemap/default.aspx">site map</a></div>
+
+                <div class="footer-content-block-no-border footer-medium"></div>
+
+				<div class="clearer"></div>
+
+			</div>
+  </footer>
+
+
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+  <script>window.jQuery || document.write('<script src="js/libs/jquery-1.7.1.min.js"><\/script>')</script>
+
+  <script src="js/plugins.js"></script>
+  <script src="js/script.js"></script>
+
+  <script>
+    var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+    g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+    s.parentNode.insertBefore(g,s)}(document,'script'));
+  </script>
+</body>
+</html>
